9a1e077406e77335d742b4aca4b974a72b01de8b
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-10 19:37:37 +0900

    java awt를 이용한 GUI 메인 구현

---
+++ src/toyProjectLotto/LottoAwtMain.java
@@ -0,0 +1,58 @@
+package toyProjectLotto;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class LottoAwtMain extends Frame {
+
+	public LottoAwtMain() {
+		
+		super("Lotto");
+		
+		Label lblLotto = new Label("Lotto!", Label.CENTER);
+		lblLotto.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
+		Label lblInfo = new Label("게임 수 입력(0~5) : ");
+		TextField tfGame = new TextField(10);
+		Button btnExe = new Button("실행");
+		Button btnExit = new Button("종료");
+		Panel panelNorth = new Panel();
+		Panel panelCenter = new Panel();
+		
+		panelNorth.add(lblLotto);
+		
+		setLayout(new GridLayout(2, 1));
+		
+		add(panelNorth);
+		add(panelCenter);
+
+		panelCenter.setLayout(new GridLayout(2, 2));
+		panelCenter.add(lblInfo);
+		panelCenter.add(tfGame);
+		panelCenter.add(btnExe);
+		panelCenter.add(btnExit);
+		
+		setBounds(300, 250, 300, 150);
+		setVisible(true);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		
+		new LottoAwtMain();
+	}
+}
