4ad6ab4a1d8e5d19cfa371b2933c2b6d17c98bd7
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-11 19:06:46 +0900

    파일명 변경

--- src/toyProjectLotto/LottoMain.java
+++ src/toyProjectLotto/LottoMain.java
@@ -8,20 +8,24 @@ import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class LottoAwtMain extends Frame {
+public class LottoMain extends Frame {
 
-	public LottoAwtMain() {
+	TextField tfGame;
+	
+	public LottoMain() {
 		
 		super("Lotto");
 		
 		Label lblLotto = new Label("Lotto!", Label.CENTER);
 		lblLotto.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
 		Label lblInfo = new Label("게임 수 입력(0~5) : ");
-		TextField tfGame = new TextField(10);
+		tfGame = new TextField(10);
 		Button btnExe = new Button("실행");
 		Button btnExit = new Button("종료");
 		Panel panelNorth = new Panel();
@@ -40,6 +44,13 @@ public class LottoAwtMain extends Frame {
 		panelCenter.add(btnExe);
 		panelCenter.add(btnExit);
 		
+		btnExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		
 		setBounds(300, 250, 300, 150);
 		setVisible(true);
 		
@@ -53,6 +64,6 @@ public class LottoAwtMain extends Frame {
 	
 	public static void main(String[] args) {
 		
-		new LottoAwtMain();
+		new LottoMain();
 	}
 }
